427c5bff8a0b47fbf889338d366724fdf275d010
Zach <Zacaframe@gmail.com>
2021-06-15 13:06:42 -0400

    Renaming files

--- timersExercise.js
+++ timersExercise.js
@@ -1,5 +1,8 @@
 // Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should log “DONE!” and stop.
+
+// Initializing our 
 let intervalId;
+
 const countDown = (num) => {
   let interval = num;
   intervalId = setInterval(function () {
@@ -13,7 +16,3 @@ const countDown = (num) => {
 };
 
 countDown(10);
-// 3
-// 2
-// 1
-// "DONE!"
